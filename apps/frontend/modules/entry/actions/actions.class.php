bde6044c42c20c863aa34ccd10cb6691ea2f6534
Anthony Regeda <anton@regeda.ru>
2011-06-24 01:47:56 +0400

    form action is created and symlink resizer is introduced

---
+++ apps/frontend/modules/entry/actions/actions.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * entry actions.
+ *
+ * @package    jim-link
+ * @subpackage entry
+ * @author     Anthony Regeda
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class entryActions extends sfActionsExt
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    if ($request->isMethod(sfRequest::POST) && isset($request['url'])) {
+      $symlink = Doctrine::getTable('Link')->createSymlink($request['url']);
+      $this->short = $symlink->id;
+      if ($request->isXmlHttpRequest()) {
+        return $this->outputJSON();
+      }
+    }
+    $this->symlinks = Doctrine::getTable('Symlink')->getFresh(sfConfig::get('app_symlink_list_size', 20));
+  }
+
+  /**
+   * Redirect short url to original
+   * @param sfWebRequest $request
+   * @return string
+   */
+  public function executeRedirect(sfWebRequest $request)
+  {
+    if (isset($request['short'])) {
+      $symlink = Doctrine::getTable('Symlink')->find($request['short']);
+      /* @var $symlink Symlink */
+      if ($symlink) {
+        $this->url = $symlink->Link->url;
+        if ($request->isXmlHttpRequest()) {
+          return $this->outputJSON();
+        }
+        else {
+          $this->redirect($this->url);
+        }
+      }
+    }
+    if ($request->isXmlHttpRequest()) {
+      $this->error = 'Url is not found';
+      return $this->outputJSON();
+    }
+    else {
+      $this->redirect404();
+    }
+  }
+}
---
+++ lib/ext/sfActionsExt.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Description of sfActionsExt
+ *
+ * @author regeda
+ */
+class sfActionsExt extends sfActions
+{
+  /**
+   * Output JSON
+   * @return string
+   */
+  static public function outputJSON()
+  {
+    $result = json_encode($this->getVarHolder()->getAll());
+    if (isset($this->request['callback']))
+    {
+      $result = $this->request['callback'].'('.$result.')';
+      $contentType = 'application/x-javascript';
+    }
+    else
+    {
+      $contentType = 'application/json';
+    }
+    $this->response->setHttpHeader('Content-Length', strlen($result));
+    $this->response->setContentType($contentType);
+    $this->response->setContent($result);
+    return sfView::NONE;
+  }
+}
